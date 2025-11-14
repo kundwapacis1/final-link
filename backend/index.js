dc428b8b043d9fdae31b2a2147249f606811c92d
kundwa pacis1 <gihozokundwa250@gmail.com>
2025-11-14 18:07:31 +0200

    the other commit

--- backend/index.js
+++ backend/index.js
@@ -70,11 +70,10 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 
 // --- MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/local-link', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 
 // --- Routes
 app.use('/api/files', fileRoutes);
